a5227248195f6a3b30289fed2dfe33a58ead72a0
babotavo419 <one.faith419@gmail.com>
2023-06-19 13:18:42 -0400

    you asked for it message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import SearchBar from './components/SearchBar';
 import DataDisplay from './components/DataDisplay';
-
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -12,19 +11,46 @@ const AppContainer = styled.div`
   padding: 2rem;
 `;
 
+const Heading = styled.h1`
+  color: #ff7f00;
+`;
+
+const MessageContainer = styled.div`
+  margin-top: 2rem;
+`;
+
+const Message = styled.p`
+  color: #ff7f00;
+`;
+
 const App = () => {
-  const { loading, error } = useSelector((state) => state);
+  const [showMessage, setShowMessage] = useState(false);
+  const { loading, error, data } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const handleSearch = (query) => {
+    dispatch(fetchData(query));
+    setShowMessage(false);
+  };
 
   return (
     <AppContainer>
-      <h1 style={{ color: '#ff7f00' }}>Enter if you dare</h1>
-      <SearchBar />
+      <Heading>Enter If You Dare</Heading>
+      <SearchBar onSearch={handleSearch} />
+      {showMessage && (
+        <MessageContainer>
+          <Message>You asked for it</Message>
+        </MessageContainer>
+      )}
       {loading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
-      <DataDisplay />
+      {data && <DataDisplay />}
     </AppContainer>
   );
 };
 
 export default App;
+
+
+
 
