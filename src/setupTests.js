0396222de9e808adfdf97d7579bcfbbe08f4f51c
babotavo419 <one.faith419@gmail.com>
2023-06-20 04:51:16 -0400

    test file

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,22 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  test('renders app components', () => {
+    render(<App />);
+
+    // Assert the presence of the heading
+    const headingElement = screen.getByText(/Enter If You Dare/i);
+    expect(headingElement).toBeInTheDocument();
+
+    // Assert the presence of the search bar
+    const searchBarElement = screen.getByPlaceholderText(/Enter search query/i);
+    expect(searchBarElement).toBeInTheDocument();
+
+    // Assert the absence of the "You asked for it" message initially
+    const messageElement = screen.queryByText(/You asked for it/i);
+    expect(messageElement).toBeNull();
+  });
+});
+;
